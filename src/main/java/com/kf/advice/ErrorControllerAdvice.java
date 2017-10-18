c2a65d867e8441d0af88b69d9e85c3351360f9c9
zhy <384108026@qq.com>
2017-10-19 00:45:33 +0800

     尝试错误页面统一处理

--- src/main/java/com/kf/advice/ErrorControllerAdvice.java
+++ src/main/java/com/kf/advice/ErrorControllerAdvice.java
@@ -1,5 +1,7 @@
 package com.kf.advice;
 
+import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -10,7 +12,7 @@ import java.util.Map;
 public class ErrorControllerAdvice {
     public static final String DEFAULT_ERROR_VIEW = "error";
 
-    @ExceptionHandler(value = NumberFormatException.class)
+    @ExceptionHandler(value = Exception.class)
     public ModelAndView defaultErrorHandler(HttpServletRequest req, Exception e) throws Exception {
         ModelAndView mav = new ModelAndView();
         mav.addObject("exception", e);
@@ -20,3 +22,4 @@ public class ErrorControllerAdvice {
 
     }
 }
+
---
+++ src/main/java/com/kf/controller/MainsiteErrorController.java
@@ -0,0 +1,23 @@
+package com.kf.controller;
+
+import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainsiteErrorController implements ErrorController {
+
+    private static final String ERROR_PATH = "/error";
+
+    @RequestMapping(value=ERROR_PATH)
+    public String handleError(){
+        return "phone/505";
+    }
+
+    @Override
+    public String getErrorPath() {
+        // TODO Auto-generated method stub
+        return ERROR_PATH;
+    }
+
+}
